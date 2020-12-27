08fcfb30344e671011ead6b9afe293e2e3d9e288
bestie06 <fatih.kgokmen@gmail.com>
2020-12-26 23:14:12 -0500

    level2 - encryption

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require("express");
  const bodyParser = require("body-parser");
  const ejs = require("ejs");
  const mongoose = require("mongoose");
+ const encrypt = require("mongoose-encryption");
+
 
  const app = express();
 
@@ -13,10 +15,13 @@ const express = require("express");
 
  mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true,useUnifiedTopology: true});
 
- const userSchema = ({
+ const userSchema = new mongoose.Schema ({
    email: String,
    password: String
  });
+
+ const secret = "Thisisourlittlesecret.";
+ userSchema.plugin(encrypt, { secret: secret, encryptedFields: ["password"] });
 
  const User = new mongoose.model("User", userSchema);
 
