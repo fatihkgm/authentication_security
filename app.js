c72215fe19b3db5e3825b9544126811baca5b12a
bestie06 <fatih.kgokmen@gmail.com>
2020-12-28 03:06:03 -0500

    Show type on data ejs secret

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use(bodyParser.urlencoded({
    resave: false,
    saveUninitialized: false
  }));
- 
+
  app.use(passport.initialize());
  app.use(passport.session());
  
@@ -35,7 +35,8 @@ app.use(bodyParser.urlencoded({
  const userSchema = new mongoose.Schema ({
   email: String,
   password: String,
-  googleId: String
+  googleId: String,
+  secret: String
  });
 
  userSchema.plugin(passportLocalMongoose);
@@ -100,12 +101,45 @@ app.get("/register", function(req, res){
  });
 
  app.get("/secrets", function(req, res){
-   if (req.isAuthenticated()){
-     res.render("secrets");
-   } else {
-     res.redirect("/login");
-   }
- });
+  User.find({"secret": {$ne: null}}, function(err, foundUsers){
+    if (err){
+      console.log(err);
+    } else {
+      if (foundUsers) {
+        res.render("secrets", {usersWithSecrets: foundUsers});
+      }
+    }
+  });
+});
+
+app.get("/submit", function(req, res){
+  if (req.isAuthenticated()){
+    res.render("submit");
+  } else {
+    res.redirect("/login");
+  }
+});
+
+app.post("/submit", function(req, res){
+  const submittedSecret = req.body.secret;
+
+//Once the user is authenticated and their session gets saved, their user details are saved to req.user.
+  // console.log(req.user.id);
+
+  User.findById(req.user.id, function(err, foundUser){
+    if (err) {
+      console.log(err);
+    } else {
+      if (foundUser) {
+        foundUser.secret = submittedSecret;
+        foundUser.save(function(){
+          res.redirect("/secrets");
+        });
+      }
+    }
+  });
+});
+
 
  app.get("/logout", function(req, res){
    req.logout();
